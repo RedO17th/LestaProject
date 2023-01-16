5449295db31c1593f4d37e62a0a18b28b38195dc
Sarat <saratovcev-1995@mail.ru>
2023-01-16 15:57:46 +0300

    Player.Part V - QuestSubSystem.Part IIII - Transfer scripts

---
+++ Assets/Scripts/Architecture/BaseQuest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class BaseQuest : MonoBehaviour
+{
+    public event Action OnCompleted;
+
+    public bool IsCompleted { get; protected set; } = false;
+
+    protected QuestSubSystem _questSubSystem = null;
+
+    public virtual void SetCompletedState() => IsCompleted = true;
+    public virtual void SetUnCompletedState() => IsCompleted = false;
+
+    public virtual void Initialize(QuestSubSystem system)
+    {
+        _questSubSystem = system;
+    }
+
+    public virtual void Prepare() { }
+    public abstract void Launch();
+
+    protected virtual void CheckCompliting()
+    {
+        if (CheckConditionOfCompliting())
+        {
+            OnCompleted?.Invoke();
+        }
+    }
+
+    protected virtual bool CheckConditionOfCompliting() { return false; }
+    public abstract void Complete();
+}
--- Assets/Scripts/Architecture/QuestSubSystem.cs
+++ Assets/Scripts/Architecture/QuestSubSystem.cs
@@ -83,34 +83,3 @@ public class QuestSubSystem : BaseSubSystem
         base.Clear();
     }
 }
-
-public abstract class BaseQuest : MonoBehaviour
-{
-    public event Action OnCompleted;
-
-    public bool IsCompleted { get; protected set; } = false;
-
-    protected QuestSubSystem _questSubSystem = null;
-
-    public virtual void SetCompletedState() => IsCompleted = true;
-    public virtual void SetUnCompletedState() => IsCompleted = false;
-
-    public virtual void Initialize(QuestSubSystem system)
-    {
-        _questSubSystem = system;
-    }
-
-    public virtual void Prepare() { }
-    public abstract void Launch();
-
-    protected virtual void CheckCompliting()
-    {
-        if (CheckConditionOfCompliting())
-        {
-            OnCompleted?.Invoke();
-        }
-    }
-
-    protected virtual bool CheckConditionOfCompliting() { return false; }
-    public abstract void Complete();
-}
